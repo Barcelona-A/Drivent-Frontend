a60141430ee419a65f80f36e96c566e8ac664785
MacenaAndre <andremacenacosta@gmail.com>
2022-12-23 00:08:58 -0300

    feat(payments): merged with main and a few adjustments

--- src/layouts/Subtitle.js
+++ src/layouts/Subtitle.js
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 
 const SubTitle = styled.h5`
+    font-family: 'Roboto', sans-serif;
     font-size: 20px;
     font-weight: 400;
     line-height: 23px;
     color: #8E8E8E;
-    margin: 15px 0px;
+    margin: 35px 0px 20px 0px;
 `;
 
 export { SubTitle };
